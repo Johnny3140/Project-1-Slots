108e61c601b60c14ad6e86a4ecf6d44cc2857dca
Johnny3140 <Johnnylopez0314@gmail.com>
2023-10-07 16:17:25 -0400

    spin and bet button in html files begginning psuedo code complete.

--- app.js
+++ app.js
@@ -2,3 +2,5 @@
 //set up bet button and array of wage amounts
 //set up function to grab random symbol from array
 //set up the spin function for spinning the lanes by calling the grab function 3 times for each slot lane
+//set up jackpot function 
+//set up win funciton to display winnings 
