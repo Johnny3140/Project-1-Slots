2962f4fb3f3c0005e6fbf692b9a73a84d8758824
Johnny3140 <Johnnylopez0314@gmail.com>
2023-10-09 19:08:16 -0400

    const set and symbol placeholders set

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //set up all conts 'symbols and spin function 
+const symbols = ['naruto','sauske','itatchi','neji','Kakashi','Sakura','Jackpot=minato'] //strings until I gather up the images for each
 //set up bet button and array of wage amounts
+const betButton = document.getElementById('betButton');
+const spinButton = document.getElementById('spinButton');
+const wagerAmounts = [1000, 2500, 5000, 10000]
 //set up function to grab random symbol from array
 //set up the spin function for spinning the lanes by calling the grab function 3 times for each slot lane
 //set up jackpot function 
